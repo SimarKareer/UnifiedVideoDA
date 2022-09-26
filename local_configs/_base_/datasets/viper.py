9233aa76c87b797a7f1a5414a30545b6d7903d09
vivekvjk <vivekvjk2020@gmail.com>
2022-09-26 13:31:21 -0400

    not sure about iamge scale - just copying cityscapes code and what was used for mapillary too

--- local_configs/_base_/datasets/viper.py
+++ local_configs/_base_/datasets/viper.py
@@ -11,7 +11,7 @@ crop_size = (720, 1280)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
-    dict(type='Resize', img_scale=(1920, 1080), ratio_range=(0.5, 2.0)),
+    dict(type='Resize', img_scale=(2048, 1024), ratio_range=(0.5, 2.0)),
     dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.75),
     dict(type='RandomFlip', prob=0.5),
     dict(type='PhotoMetricDistortion'),
@@ -25,7 +25,7 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         #orig image size of viper
-        img_scale=(1920, 1080),
+        img_scale=(2048, 1024),
         # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
