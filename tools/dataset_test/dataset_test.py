2ce7745fda868eac8db2d5ea46a62e664ca47586
Simar Kareer <skareer@outlook.com>
2023-04-13 18:41:09 -0400

    flow flow stats target vs source

--- tools/dataset_test/dataset_test.py
+++ tools/dataset_test/dataset_test.py
@@ -122,7 +122,7 @@ def get_csseq():
 
 
 
-def main():
+def data_loader_test():
     # data_loader = DataLoader(viper)
     dataset = get_viper()
     dataset.data_type = "rgb+flow"
@@ -154,5 +154,64 @@ def main():
 
         if i == 2:
             break
+
+def flow_statistics_gt():
+    """
+    calculate the distribution of flow magnitudes per class based on ground truth
+    """
+    NUM_ITERS=500
+    
+    def get_per_class_hist(dataset, num_iters=100):
+        data_loader = DataLoader(
+            dataset,
+            collate_fn=partial(collate, samples_per_gpu=1),
+            num_workers=3,
+            shuffle=True
+        )
+
+        per_class_hist = torch.zeros((19, 100))
+
+        for i, data in enumerate(tqdm(data_loader)):
+            img, _, flow, gt_t, _ = data["img"][0].numpy(), data["imtk"][0], data["flow"][0], data["gt_semantic_seg"][0], data["imtk_gt_semantic_seg"][0]
+
+            for j in range(19):
+                per_class_hist[j] += torch.histc(flow.norm(dim=0)[(gt_t == j)[0]], bins=100, min=0, max=500)
+
+
+            if i == num_iters:
+                break
+        
+        return per_class_hist
+    
+    def stacked_hist_multiclass(per_class_hist1, per_class_hist2):
+        """
+        visalize a stacked bar chart for each class in per_class_hist1 and per_class_hist2, which clearly shows both the larger and smaller values
+        """
+        fig, axs = plt.subplots(19, 1, figsize=(10, 100))
+        for i, class_name in enumerate(CityscapesSeqDataset.CLASSES):
+            axs[i].bar(np.arange(100), per_class_hist1[i], label="Source", alpha=0.5)
+            axs[i].bar(np.arange(100), per_class_hist2[i], label="Target", alpha=0.5)
+            axs[i].set_title(class_name, fontsize = '25')
+            axs[i].legend()
+        fig.savefig(f"/coc/testnvme/skareer6/Projects/VideoDA/mmsegmentation/work_dirs/flowHistograms/hist.png")
+    
+    
+    viper = get_viper()
+    csseq = get_csseq()
+    viper.data_type = "rgb+flow"
+    csseq.data_type = "rgb+flow"
+
+    per_class_hist_source, per_class_hist_target = get_per_class_hist(viper, num_iters=NUM_ITERS), get_per_class_hist(csseq, num_iters=NUM_ITERS)
+    stacked_hist_multiclass(per_class_hist_source, per_class_hist_target)
+    # double_per_class_histograms([2, 1], [1, 2])
+    # stacked_histogram([2, 1], [1, 2])
+
+
+
+    
+def main():
+    # data_loader_test()
+
+    flow_statistics_gt()
 
 main()
