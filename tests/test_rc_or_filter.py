95463f1ac5894ba57cc7358f49542a6997727d50
vivekvjk <vivekvjk2020@gmail.com>
2023-04-14 16:24:36 -0400

    test for rare_common class compare in or filter

--- tests/test_rc_or_filter.py
+++ tests/test_rc_or_filter.py
@@ -73,9 +73,89 @@ def main():
     filtered = rare_class_or_filter(torch.stack([pl1, pl1]), torch.stack([pl2, pl2]))
     assert(torch.all(filtered == torch.stack([ans, ans])))
 
+
+def rare_common_class_or_filter():
+
+    print("Test 1")
+    pl1 = torch.tensor([
+        [9, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    
+    pl2 = torch.tensor([
+        [0, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    ans = torch.tensor([
+        [255, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+
+
+    filtered = rare_class_or_filter(pl1, pl2, rare_common_compare=True)
+    assert(torch.all(filtered == ans))
+
+    print("Test 2")
+
+    pl1 = torch.tensor([
+        [3, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    
+    pl2 = torch.tensor([
+        [5, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    ans = torch.tensor([
+        [255, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+
+    filtered = rare_class_or_filter(pl1, pl2, rare_common_compare=True)
+    assert(torch.all(filtered == ans))
+
+    print("Test 3")
+    pl1 = torch.tensor([
+        [13, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    
+    pl2 = torch.tensor([
+        [5, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    ans = torch.tensor([
+        [5, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+
+    filtered = rare_class_or_filter(pl1, pl2, rare_common_compare=True)
+    assert(torch.all(filtered == ans))
+
+    print("Test 4")
+    pl1 = torch.tensor([
+        [3, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    
+    pl2 = torch.tensor([
+        [13, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+    ans = torch.tensor([
+        [3, 18, 1, 2],
+        [15, 4, 10, 0]
+    ])
+
+    filtered = rare_class_or_filter(pl1, pl2, rare_common_compare=True)
+    assert(torch.all(filtered == ans))
+    
+    print("All good")
+
     
 
     # print(filtered)
 
 
-main()
+# main()
+rare_common_class_or_filter()
