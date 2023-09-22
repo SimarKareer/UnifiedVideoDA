e9ca13f64079f469887a1dd9a577777901041b57
vivekvjk <vivekvjk2020@gmail.com>
2023-09-21 23:59:27 -0400

    add updated bash files for experiments

---
+++ tools/experiments/synthiaSeq_csSeq/mic_hrda_component_ablation/synthiaSeq_csseq_hrda_dlv2_no_MRFusion_no_rcs.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+EXP_NAME=synthiaSeq_csSeq_hrda_dlv2_no_MRFusion_no_rcs
+python ./tools/train.py configs/mic/synthiaSeqHR2csHR_mic_hrda_deeplab_no_MRFusion_no_rcs.py --launcher=slurm --no-masking True --l-warp-lambda=0.0 --l-mix-lambda=1.0 --seed 1 --deterministic --work-dir=./work_dirs/synthia/$EXP_NAME --auto-resume True --wandbid $EXP_$T
